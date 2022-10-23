1bbc2c0059d9f31863ee05fb9a2a99b0918c6157
novenopatch <novenosexta77@gmail.com>
2022-10-23 19:50:26 +0000

    base usage of sg

--- Converter/setup.py
+++ Converter/setup.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
+import sys
 layout =[
-    [sg.Text("Text"),sg.Spin(['item1','item2','item3','item4',])],
-    [sg.Button("Button")],
-    [sg.Input()],
-    [sg.Text('Test'),sg.Button("Test Button")],
+    [sg.Text("Text",enable_events = True, key="-TEXT-"),sg.Spin(['item1','item2','item3','item4',])],
+    [sg.Button("Button",key='-BUTTON1-')],
+    [sg.Input(key="-Input-")],
+    [sg.Text('Test'),sg.Button("Button",key='-BUTTON2-')],
 ]
 window = sg.Window('Converter',layout)
 
@@ -11,4 +12,10 @@ while True :
     event, values = window.read()
     if event == sg.WINDOW_CLOSED:
         break
+    if event == "-BUTTON1-":
+        window["-TEXT-"].update(values["-Input-"])
+        print(values)
+    if event == "-TEXT-":
+            print('text was pressed')
 window.close()
+sys.exit()
